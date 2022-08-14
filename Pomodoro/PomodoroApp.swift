87fd3485bbeb7ac46995879586bd7ce2af85ce51
alfiecg24 <alfiecockell@outlook.com>
2022-08-14 18:47:59 +0100

    Initial Commit

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Alfie on 14/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
