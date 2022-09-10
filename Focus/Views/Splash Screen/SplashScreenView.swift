1d100326c8ee72e2cc398931b4f0783d59eabd45
alfiecg24 <alfiecockell@outlook.com>
2022-09-10 10:26:15 +0100

    Missing SplashScreenView?

---
+++ Focus/Views/Splash Screen/SplashScreenView.swift
@@ -0,0 +1,40 @@
+//
+//  SplashScreenView.swift
+//  Focus
+//
+//  Created by Alfie on 09/09/2022.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State private var isFinished = false
+    var body: some View {
+        VStack(alignment: .center) {
+            
+            Spacer()
+            
+            TitleView()
+            
+            InformationContainerView()
+            
+            Spacer()
+            
+            Button(action: {
+                let generator = UINotificationFeedbackGenerator()
+                generator.notificationOccurred(.success)
+                isFinished.toggle()
+                UserDefaults.standard.set(true, forKey: "hasLaunchedBefore")
+            }) {
+                Text("Continue")
+                    .customButton()
+            }
+            .padding(.horizontal)
+            .fullScreenCover(isPresented: $isFinished, content: {
+                MainView()
+            })
+            
+            Spacer()
+        }
+    }
+}
